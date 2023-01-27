0edbc7777ff421bd973a639ad1fa7e58b37a18c1
abel <se.abel.tadesse@gmail.com>
2023-01-27 19:10:33 +0300

    Singleton with RunEnv and with Single Demo Singleton

---
+++ Java/Singleton/src/DbSingleton.java
@@ -0,0 +1,11 @@
+public class DbSingleton {
+
+    private static DbSingleton instance = new DbSingleton();
+
+    private DbSingleton(){
+
+    }
+    public static DbSingleton getInstance(){
+        return instance;
+    }
+}
---
+++ Java/Singleton/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Runtime Env
+//        Runtime sinletonRunTime = Runtime.getRuntime();
+//        sinletonRunTime.gc();
+//        System.out.println(sinletonRunTime);
+//        Runtime anotherInstance = Runtime.getRuntime();
+//        System.out.println(anotherInstance);
+//        if(sinletonRunTime == anotherInstance){
+//            System.out.println("They are the same instance");
+//        }
+
+    }
+}
