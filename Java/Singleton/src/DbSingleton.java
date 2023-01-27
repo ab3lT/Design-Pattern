56cb4f1778a0ff62a60d1d74f38035bce0688096
abel <se.abel.tadesse@gmail.com>
2023-01-27 19:18:51 +0300

    lazy loading singleton

--- Java/Singleton/src/DbSingleton.java
+++ Java/Singleton/src/DbSingleton.java
@@ -1,11 +1,19 @@
 public class DbSingleton {
 
-    private static DbSingleton instance = new DbSingleton();
+    //private static DbSingleton instance = new DbSingleton();
+
+
+    // lazy loading
+    private static DbSingleton instance = null;
 
     private DbSingleton(){
 
     }
     public static DbSingleton getInstance(){
+        //lazy  loaded
+        if(instance == null){
+            instance =  new DbSingleton();
+        }
         return instance;
     }
 }
